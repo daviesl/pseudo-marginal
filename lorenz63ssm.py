Fixed some outstanding bugs on the scale of process noise - there was a missing matrix multiplication in the diffusion bridges. Also fixed an off-by-one bug where the y vector was misaligned to the state vector.

lorenz63ssm.py
@@ -63,7 +63,8 @@ class Lorenz63Abstract(ItoProcess):
         return 40
     @classmethod
     def obserr(cls):
-        return 0.1
+        #return 0.1
+        return 0.49
     @classmethod
     def delta_t(cls):
         return 0.001
@@ -100,11 +101,11 @@ class Lorenz63Abstract(ItoProcess):
     @staticmethod
     @numba.jit 
     def transformXtoState(X):
-        return np.column_stack((X[:,0]*20.0,X[:,1]*20.0,X[:,2]*5.+0.8))
+        return np.column_stack((X[...,0]*20.0,X[...,1]*20.0,X[...,2]*5.+0.8))
     @staticmethod
     @numba.jit 
     def transformStatetoX(tr):
-        return np.column_stack((tr[:,0]/20.0,tr[:,1]/20.0,(tr[:,2]-0.8)/5.))
+        return np.column_stack((tr[...,0]/20.0,tr[...,1]/20.0,(tr[...,2]-0.8)/5.))
     @staticmethod
     @numba.jit
     def observationEquation(Xs): 
@@ -121,8 +122,8 @@ class Lorenz63Abstract(ItoProcess):
         return Ys
     @classmethod
     def initialState(cls):
-        #return np.array([-2.32,0.771,25.6])
-        return np.array([0.,1.,1.05])
+        return np.array([-2.32,0.771,25.6])
+        #return np.array([0.,1.,1.05])
     @classmethod
     def synthetic(cls,dt=0.001,num_steps=10000,th0=theta0):
         x0=cls.initialState()

lorenz96ssm.py
@@ -10,10 +10,14 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import sys
 
 theta0 = np.array([8.])
-om = np.zeros((36,3),dtype=np.float64)
-om[0,0]=1
-om[36//2,1]=1
-om[36-1,2]=1
+nL = 10
+x0 = np.ones(nL)*theta0[0]
+x0[-1]+=0.01
+om = np.zeros((nL,nL//2),dtype=np.float64)
+j=0
+for i in range(0,nL,2):
+    om[i,j]=1
+    j+=1
 
 class Lorenz96Abstract(ItoProcess):
     @classmethod
@@ -25,17 +29,24 @@ class Lorenz96Abstract(ItoProcess):
         return cls.transformParameterstoTheta(theta0)
     @classmethod
     def X_size(self):
-        return 36
+        global nL
+        return nL
+    @classmethod
+    def initialState(cls):
+        global x0
+        return x0
     @classmethod
     def theta_size(self):
         return 1
     @classmethod
     def y_dim(cls):
         # TODO make this DRY with obs_map.ndim
-        return 3
+        global om
+        return om.shape[1]
     @classmethod
     def obsinterval(cls):
-        return 40
+        #return 40
+        return 20
     @classmethod
     #@numba.jit
     def obserr(cls):
@@ -43,6 +54,11 @@ class Lorenz96Abstract(ItoProcess):
     @classmethod
     def delta_t(cls):
         return 0.001
+    @staticmethod
+    @numba.jit
+    def diffusion(X_k,y_J,theta):
+        dim=X_k.shape[-1]
+        return np.eye(dim) * 0.01
     @classmethod
     def observationCovariance(cls):
         return np.eye(cls.y_dim())*(cls.obserr()**2)
@@ -117,10 +133,13 @@ class Lorenz96Abstract(ItoProcess):
     @classmethod
     def plot_synthetic(cls,Xs,Ys,num_steps):
         ff,ax=plt.subplots()
-        ax.plot(Xs[:,0],'r',label="x",lw=1)
-        ax.plot(Xs[:,1],'g',label="y",lw=1)
-        ax.plot(Xs[:,2],'b',label="z",lw=1)
-        ax.plot(np.arange(0,num_steps+1,cls.obsinterval()),Ys[:,0],'r+',np.arange(0,num_steps+1,cls.obsinterval()),Ys[:,1],'b+',label="Observed",lw=1)
+        clr=['#e6194B','#f58231','#ffe119','#bfef45','#3cb44b','#42d4f4','#4363d8','#911eb4','#f032e6','#a9a9a9']
+        # do colours?
+        for i in range(Xs.shape[1]):
+            ax.plot(Xs[:,i],clr[i],label="X_{}".format(i),lw=1)
+        # todo fix y colours by using obsmap matrix on np.arange(X_size()) indices
+        for i in range(Ys.shape[1]):
+            ax.plot(np.arange(0,num_steps+1,cls.obsinterval()),Ys[:,i],color=clr[2*i],marker='+',label="Y_{}".format(i),lw=1)
         ax.legend()
         plt.show()
         fig = plt.figure()

pm_run.py
@@ -12,20 +12,6 @@ from lorenz63ssm import Lorenz63Abstract
 from lorenz96ssm import Lorenz96Abstract
 from kuramotossm import KuramotoAbstract
 
-#class Lorenz63(LindstromBridge,Lorenz63Abstract):
-#    pass
-#class Lorenz96(LindstromBridge,Lorenz96Abstract):
-#    pass
-class Lorenz96(Lorenz96Abstract):
-    pass
-class Lorenz63(Lorenz63Abstract):
-    pass
-class Kuramoto(KuramotoAbstract):
-    pass
-
-
-
-
 if __name__ == '__main__':
     timestr = time.strftime("%Y%m%d-%H%M%S")
 
@@ -34,25 +20,55 @@ if __name__ == '__main__':
 
     modelflag = sys.argv[argctr].lower()
     argctr += 1
+
+    bridgeflag = sys.argv[argctr].lower()
+    argctr += 1
+
+    pfflag = sys.argv[argctr].lower()
+    argctr += 1
     
     if modelflag == 'lorenz96':
-        class ModelClass(Lorenz96):
+        class AbstractModelClass(Lorenz96Abstract):
             pass
     elif modelflag == 'kuramoto':
-        class ModelClass(Kuramoto):
+        class AbstractModelClass(KuramotoAbstract):
             pass
     else: # Lorenz63
-        class ModelClass(Lorenz63):
+        class AbstractModelClass(Lorenz63Abstract):
+            pass
+
+    if bridgeflag == 'mdb':
+        print("Using the Modified Diffusion Bridge")
+        class ModelClass(ModifiedDiffusionBridge,AbstractModelClass):
+            pass
+    elif bridgeflag == 'res':
+        print("Using the Residual Bridge")
+        class ModelClass(ResidualBridge,AbstractModelClass):
+            pass
+    elif bridgeflag == 'lin':
+        print("Using the Lindstrom Bridge")
+        class ModelClass(LindstromBridge,AbstractModelClass):
+            pass
+    elif bridgeflag == 'reslin':
+        print("Using the Residual Lindstrom Bridge")
+        class ModelClass(LindstromResidualBridge,AbstractModelClass):
+            pass
+    else: # none
+        print("Using Myopic Euler Maruyama")
+        class ModelClass(AbstractModelClass):
             pass
 
     print("Model being used is {}".format(ModelClass.who()))
-    synthetic_name = "{}_synthetic".format(ModelClass.who())
+    synthetic_name = "{}_{}_synthetic".format(ModelClass.who(),bridgeflag)
     theta0=ModelClass.transformThetatoParameters(ModelClass.default_theta())
+
+    nflag = int(sys.argv[argctr])
+    argctr += 1
 
     actionflag = sys.argv[argctr]
     argctr += 1
 
-    num_steps = 6400 #3200 #12800 #12800 #3200 #12800 # 1600
+    num_steps = 1200 #12800 #1600 #6400 #3200 #12800 #12800 #3200 #12800 # 1600
     #X0_ = ModelClass.initialState() #np.array([0,1,1.05])
     #X0_ = X0_[np.newaxis,:]
     #X0_mu = ModelClass.transformStatetoX(np.array([[0,1,1.05],[0,1,1.05]]))
@@ -75,25 +91,17 @@ if __name__ == '__main__':
         print("X0_mu = {}".format(X0_mu))
         print("Theta = {}".format(theta0))
 
-        n=8192 #2048 #1024 #8192 #1024 #16384 #2048 #512
-        chain_length=100
+        n=nflag #16384 #1024 #8192 #1024 #16384 #2048 #512
+        chain_length=200
 
-        #pf = stateFilter(ModelClass(),Y,n)
-        #pf = ESSPartiallyAdaptedParticleFilter(ModelClass(),Y,n)
-        pf = auxiliaryParticleFilter(ModelClass(),Y,n)
+        if pfflag=='apf':
+            pf = auxiliaryParticleFilter(ModelClass(),Y,n)
+        elif pfflag=='essapf':
+            pf = ESSPartiallyAdaptedParticleFilter(ModelClass(),Y,n)
+        else: # bootstrap
+            pf = stateFilter(ModelClass(),Y,n)
 
         # run pmmh
-        #sampler = pmpfl(innov_lindstrom_bridge,innov_lindstrom_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov,innov_lindstrom_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov_lindstrom_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov_diffusion_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov_lindstrom_bridge,propagate_noisefree,lh,Y,3,3,n)
-        #sampler = pmpfl(innov_lindstrom_residual_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov,propagate_noisefree,lh,Y,3,3,n)
-        #sampler = pmpfl(innov,locally_opt_proposal_lindstrom_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov_residual_bridge,innov,lh,Y,3,3,n)
-        #sampler = pmpfl(innov,innov_lindstrom_residual_bridge,lh,Y,3,3,n)
-        #sampler = pmpfl(innov,innov,lh,Y,3,3,n)
         sampler = parameterEstimator(pf)
 
     if actionflag == 't':
@@ -128,12 +136,14 @@ if __name__ == '__main__':
         ml_test_log_var = 2. * ml_test_log_mean + np.log(np.sum((np.exp(ml_test-ml_test_log_mean)-1)**2)) - math.log(chain_length)
         
         
+        print("Synthetic is {}".format(synthetic_name))
         print("Log Marginal likelihood: Mean = {} Std Dev = {}".format(ml_test.mean(),ml_test.std()))
         print("Log Non-log: Mean = {} Std Dev = {}".format(ml_test_log_mean,ml_test_log_var * 0.5))
     
         fig,(ax1,ax2) = plt.subplots(1,2)
         ax1.plot(ml_test,'y',linewidth=1)
         ax2.boxplot(ml_test)
+        fig.suptitle("{} {}".format(r"$ln(Var[p(y|\theta)])$)",synthetic_name), fontsize=16)
         plt.show()
 
     elif actionflag == 'r':